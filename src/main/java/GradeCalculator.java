15be1526d88f9313cccd25b326221e18e16c2450
Brian B <brianb@MacBook-Pro-4.local>
2019-02-14 16:19:55 -0500

    Refactor of tests

--- src/main/java/GradeCalculator.java
+++ src/main/java/GradeCalculator.java
@@ -1,43 +1,78 @@
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 
 public class GradeCalculator {
 
+    DecimalFormat decimalFormat = new DecimalFormat("###.00");
 
-    public String getLetterGrade(int percentage){
+    public String getLetterGrade(int percentage) {
 
-      return checkPercentageForGrade(percentage);
+        return checkPercentageForGrade(percentage);
 
     }
 
-    public int getAveragePercentageOfGrades(ArrayList<Integer> grades){
+    public int getAveragePercentageOfGrades(ArrayList<Integer> grades) {
 
         int gradeSum = 0;
 
-        for(Integer grade: grades){
+        for (Integer grade : grades) {
             gradeSum += grade;
         }
 
-        try{
-
-            return gradeSum/grades.size();
-
-        } catch(ArithmeticException ex){
+        try {
+            return gradeSum / grades.size();
+        } catch (ArithmeticException ex) {
             ex.printStackTrace();
             return 0;
         }
     }
 
-    private String checkPercentageForGrade(int percentage){
-        if(percentage <= 100  && percentage > 89){
+    public double getGPAForLetterGrade(String letterGrade) {
+        return getGPAEquivalentOfLetter(letterGrade);
+    }
+
+    public double getAverageGradePointAverageForLetterGrades(ArrayList<String> letterGrades) {
+
+        double gradeSum = 0.00;
+
+        for (String grade : letterGrades) {
+            gradeSum += getGPAForLetterGrade(grade);
+        }
+
+        try {
+            return gradeSum / letterGrades.size();
+        } catch (ArithmeticException ex) {
+            ex.printStackTrace();
+            return 0.00;
+        }
+
+    }
+
+    private String checkPercentageForGrade(int percentage) {
+        if (percentage <= 100 && percentage > 89) {
             return "A";
-        } else if (percentage <= 89 && percentage > 79){
+        } else if (percentage <= 89 && percentage > 79) {
             return "B";
-        } else if (percentage <= 79 && percentage > 69){
+        } else if (percentage <= 79 && percentage > 69) {
             return "C";
-        } else if (percentage <= 69 && percentage > 59){
+        } else if (percentage <= 69 && percentage > 59) {
             return "D";
         } else {
             return "F";
+        }
+    }
+
+    private double getGPAEquivalentOfLetter(String letterGrade) {
+        if (letterGrade.equals("A-") || letterGrade.equals("A") || letterGrade.equals("A+")) {
+            return 4.00;
+        } else if (letterGrade.equals("B-") || letterGrade.equals("B") || letterGrade.equals("B+")) {
+            return 3.00;
+        } else if (letterGrade.equals("C-") || letterGrade.equals("C") || letterGrade.equals("C+")) {
+            return 2.00;
+        } else if (letterGrade.equals("D-") || letterGrade.equals("D") || letterGrade.equals("D+")) {
+            return 1.00;
+        } else {
+            return 0.00;
         }
     }
 
--- src/test/java/GradeCalculatorTest.java
+++ src/test/java/GradeCalculatorTest.java
@@ -2,6 +2,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import static org.junit.Assert.*;
 
@@ -11,54 +12,97 @@ public class GradeCalculatorTest {
     GradeCalculator gradeCalculator;
 
     @Before
-    public void create(){
-         gradeCalculator = new GradeCalculator();
-
+    public void create() {
+        gradeCalculator = new GradeCalculator();
     }
 
     @Test
-    public void shouldReturnLetterGradeWhenGivenPercentage(){
+    public void shouldReturnLetterGradeWhenGivenPercentage() {
         int firstGradePercentage = 87;
         int secondGradePercentage = 100;
         int thirdGradePercentage = 0;
 
-         String letterGrade = gradeCalculator.getLetterGrade(firstGradePercentage);
-         assertEquals("B", letterGrade);
+        String letterGrade = gradeCalculator.getLetterGrade(firstGradePercentage);
+        assertEquals("B", letterGrade);
 
-         String secondLetterGrade = gradeCalculator.getLetterGrade(secondGradePercentage);
-         assertEquals("A", secondLetterGrade);
+        String secondLetterGrade = gradeCalculator.getLetterGrade(secondGradePercentage);
+        assertEquals("A", secondLetterGrade);
 
-         String thirdLetterGrade = gradeCalculator.getLetterGrade(thirdGradePercentage);
-         assertEquals("F", thirdLetterGrade);
+        String thirdLetterGrade = gradeCalculator.getLetterGrade(thirdGradePercentage);
+        assertEquals("F", thirdLetterGrade);
     }
 
     @Test
-    public void shouldReturnAveragePercentWhenGivenCollectionOfPercentageGrades(){
+    public void shouldReturnAveragePercentWhenGivenCollectionOfPercentageGrades() {
         int expectedAveragePercentage = 10;
-        int expectedZeroForAllGrades = 0;
 
         ArrayList<Integer> grades = new ArrayList<>();
         grades.add(10);
         grades.add(15);
         grades.add(5);
 
-         int actualAveragePercentage = gradeCalculator.getAveragePercentageOfGrades(grades);
+        int actualAveragePercentage = gradeCalculator.getAveragePercentageOfGrades(grades);
 
-         assertEquals(expectedAveragePercentage, actualAveragePercentage);
-
-
-         ArrayList<Integer> zeroGrades = new ArrayList<>();
-         grades.add(0);
-         grades.add(0);
-         grades.add(0);
-
-         int actualZeroForAllGrades = gradeCalculator.getAveragePercentageOfGrades(zeroGrades);
-
-         assertEquals(expectedZeroForAllGrades, actualZeroForAllGrades);
-
+        assertEquals(expectedAveragePercentage, actualAveragePercentage);
     }
 
+    @Test
+    public void shouldReturnZeroIfAllGradesAreZero() {
+
+        int expectedZeroForAllGrades = 0;
+
+        ArrayList<Integer> zeroGrades = new ArrayList<>();
+        zeroGrades.add(0);
+        zeroGrades.add(0);
+        zeroGrades.add(0);
+
+        int actualZeroForAllGrades = gradeCalculator.getAveragePercentageOfGrades(zeroGrades);
+
+        assertEquals(expectedZeroForAllGrades, actualZeroForAllGrades);
+    }
+
+    @Test
+    public void shouldReturnGPAEquivalentForSingleLetterGrade() {
+        double expectedGPA = 4.0;
+        double actualGPA = gradeCalculator.getGPAForLetterGrade("A");
+
+        assertEquals(expectedGPA, actualGPA, 0.01);
+    }
+
+    @Test
+    public void shouldReturnZeroIfLetterGradeEqualsZero() {
+        double expectedZeroGPA = 0.00;
+        double actualZeroGPA = gradeCalculator.getGPAForLetterGrade("G");
+
+        assertEquals(expectedZeroGPA, actualZeroGPA, 0.001);
+    }
+
+    @Test
+    public void shouldReturnAverageGPA_ForMultipleLetterGrades() {
+        double expectedAverageGPA = 3.00;
+        ArrayList<String> letterGrades = new ArrayList<>(Arrays.asList("B", "A+", "C-"));
+
+        double actualAverageGPA = gradeCalculator.getAverageGradePointAverageForLetterGrades(letterGrades);
 
 
+        double expectedSecondAverageGPA = 2.60;
+        ArrayList<String> secondLetterGrades = new ArrayList<>(Arrays.asList("B", "A+", "C-", "D+", "B-"));
+
+        double secondActualAverageGPA = gradeCalculator.getAverageGradePointAverageForLetterGrades(secondLetterGrades);
+
+        assertEquals(expectedAverageGPA, actualAverageGPA, 0.001);
+        assertEquals(expectedSecondAverageGPA, secondActualAverageGPA, 0.001);
+    }
+
+    @Test
+    public void shouldReturnAnAverageOfZeroIfAllLetterGradesSumToZero() {
+
+        double expectedZeroAverageGPA = 0.00;
+        ArrayList<String> zeroLetterGrades = new ArrayList<>(Arrays.asList("G", "F", "Z", "F+"));
+
+        double actualZeroAverageGPA = gradeCalculator.getAverageGradePointAverageForLetterGrades(zeroLetterGrades);
+
+        assertEquals(expectedZeroAverageGPA, actualZeroAverageGPA, 0.001);
+    }
 
 }
